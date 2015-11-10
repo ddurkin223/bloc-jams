34bf48e2d89c297d8d844f8f162bb1534a5844d2
Dan Durkin <daniel.h.durkin@gmail.com>
2015-11-09 21:25:12 -0500

    Minor Edits

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,6 +2,6 @@ var animatePoints = function () {
 	var points = document.getElementsByClassName('point');
 	
 	for (var i = 0, len = points.length; i<len;i++) {
-		points[i].style.animation = "all 7s ease-in-out 0s 1";
+		points[i].style.animation = "all 7s ease-in-out 0s 2";
 	}
 }
