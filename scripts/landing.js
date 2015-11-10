2ca0c86ca91f0c106aaaf029ad1034a7a08b2693
Dan Durkin <daniel.h.durkin@gmail.com>
2015-11-09 21:51:16 -0500

    Completed Checkpoint 23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,22 @@
-var animatePoints = function () {
-	var points = document.getElementsByClassName('point');
-	
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function (points) {
 	for (var i = 0, len = points.length; i<len;i++) {
-		points[i].style.animation = "all 7s ease-in-out 0s 2";
+		points[i].style.animation = "all 4s ease-in-out 0s 1";
 	}
+}
+
+window.onload = function () {
+	if(window.innerHeight > 950){
+		animatePoints(pointsArray);
+	}
+	
+	var sellingPoints = document.getElementsByClassName('selling-points')[0];
+	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 100;
+	
+	window.addEventListener('scroll', function(event){
+		if(document.body.scrollTop >= scrollDistance){
+			animatePoints(pointsArray);
+		}
+	});
 }
