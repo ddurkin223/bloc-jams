290ad14582a9472a33d7b89259f5d7755ed0c8a3
Dan Durkin <daniel.h.durkin@gmail.com>
2015-11-09 21:23:40 -0500

    Playing around with Animation

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,7 @@
-(var revealPoints = function () {
+var animatePoints = function () {
 	var points = document.getElementsByClassName('point');
-
-	for(var i = 0, len = points.length; i < len; i++){
-		point[i].style.opacity = 1;
-		points[i].style.transform = "scaleX(1) translateY(0)";
-		points[i].style.mstransform = "scaleX(1) translateY(0)";
-		points[i].style.webkittransform = "scaleX(1) translateY(0)";
+	
+	for (var i = 0, len = points.length; i<len;i++) {
+		points[i].style.animation = "all 7s ease-in-out 0s 1";
 	}
-}());
+}
