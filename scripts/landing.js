d01376e3cc86e4f67b289f2994e98ca06b9f78cb
Dan Durkin <daniel.h.durkin@gmail.com>
2015-11-13 17:24:53 -0500

    Minor edits

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,15 @@
-var pointsArray = document.getElementsByClassName('point');
+var pointArray = document.getElementsByClassName('point');
 
 var animatePoints = function (points) {
 	for (var i = 0, len = points.length; i<len;i++) {
 		points[i].style.animation = "all 4s ease-in-out 0s 1";
+		points[i].style.opacity = "1";
 	}
 }
 
 window.onload = function () {
 	if(window.innerHeight > 950){
-		animatePoints(pointsArray);
+		animatePoints(pointArray);
 	}
 	
 	var sellingPoints = document.getElementsByClassName('selling-points')[0];
@@ -16,7 +17,7 @@ window.onload = function () {
 	
 	window.addEventListener('scroll', function(event){
 		if(document.body.scrollTop >= scrollDistance){
-			animatePoints(pointsArray);
+			animatePoints(pointArray);
 		}
 	});
 }
