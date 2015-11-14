d7cdf7702fa29ed2ef0627c337804fed86baa875
Dan Durkin <daniel.h.durkin@gmail.com>
2015-11-14 17:44:02 -0500

    Completed Assignment 25

--- scripts/album.js
+++ scripts/album.js
@@ -1,63 +1,98 @@
 var albumPicasso = {
-     name: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { name: 'Blue', length: '4:26' },
-         { name: 'Green', length: '3:14' },
-         { name: 'Red', length: '5:01' },
-         { name: 'Pink', length: '3:21'},
-         { name: 'Magenta', length: '2:15'}
-     ]
- };
- 
- var albumMarconi = {
-     name: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { name: 'Hello, Operator?', length: '1:01' },
-         { name: 'Ring, ring, ring', length: '5:01' },
-         { name: 'Fits in your pocket', length: '3:21'},
-         { name: 'Can you hear me now?', length: '3:14' },
-         { name: 'Wrong phone number', length: '2:15'}
-     ]
- };
+name: 'The Colors',
+	artist: 'Pablo Picasso',
+	label: 'Cubism',
+	year: '1881',
+	albumArtUrl: 'assets/images/album_covers/01.png',
+	songs: [
+		{ name: 'Blue', length: '4:26' },
+		{ name: 'Green', length: '3:14' },
+		{ name: 'Red', length: '5:01' },
+		{ name: 'Pink', length: '3:21'},
+		{ name: 'Magenta', length: '2:15'}
+	]
+};
 
- var createSongRow = function(songNumber, songName, songLength) {
-     var template =
-        '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
-      + '  <td class="song-item-title">' + songName + '</td>'
-      + '  <td class="song-item-duration">' + songLength + '</td>'
-      + '</tr>';
- 
-     return template;
- };
+var albumMarconi = {
+	name: 'The Telephone',
+	artist: 'Guglielmo Marconi',
+	label: 'EM',
+	year: '1909',
+	albumArtUrl: 'assets/images/album_covers/20.png',
+	songs: [
+		{ name: 'Hello, Operator?', length: '1:01' },
+		{ name: 'Ring, ring, ring', length: '5:01' },
+		{ name: 'Fits in your pocket', length: '3:21'},
+		{ name: 'Can you hear me now?', length: '3:14' },
+		{ name: 'Wrong phone number', length: '2:15'}
+	]
+};
 
- var setCurrentAlbum = function(album) {
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-     albumTitle.firstChild.nodeValue = album.name;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
- 
-     albumSongList.innerHTML = '';
- 
-     for (var i = 0, len = album.songs.length; i < len; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
-     }
- };
- 
- window.onload = function() {
-     setCurrentAlbum(albumPicasso);
- };
+var albumDivers = {
+	name: 'Divers',
+	artist: 'Joanna Newsom',
+	label: 'Drag City',
+	year: '2015',
+	albumArtUrl: 'assets/images/album_covers/divers.jpg',
+	songs: [
+		{ name: 'Anecdotes', length: '6:27' },
+		{ name: 'Sapokanikan', length: '5:11' },
+		{ name: 'Leaving the City', length: '3:48' },
+		{ name: 'Goose Eggs', length: '5:01' },
+		{ name: 'Walktz of the 101st Lightborne', length: '5:22' },
+		{ name: 'The Things I Say', length: '2:25' },
+		{ name: 'Divers', length: '7:07' },
+		{ name: 'Same Old Man', length: '2:26' },
+		{ name: 'You Will Not Take My Heart Alive', length: '4:01' },
+		{ name: 'A Pin-Light Bent', length: '4:26' },
+		{ name: 'Time, As a Symptom', length: '5:38' }
+	]
+};
+
+var createSongRow = function(songNumber, songName, songLength) {
+	var template =
+		'<tr class="album-view-song-item">'
+	+ 		'<td class="song-item-number">' + songNumber + '</td>'
+	+		'<td class="song-item-title">' + songName + '</td>'
+	+		'<td class="song-item-duration">' + songLength + '</td>'
+	+	'</tr>';
+
+	return template;
+};
+
+var setCurrentAlbum = function(album) {
+	var albumTitle = document.getElementsByClassName('album-view-title')[0];
+	var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+	var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+	var albumImage = document.getElementsByClassName('album-cover-art')[0];
+	var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+	albumTitle.firstChild.nodeValue = album.name;
+	albumArtist.firstChild.nodeValue = album.artist;
+	albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+	albumImage.setAttribute('src', album.albumArtUrl);
+
+	albumSongList.innerHTML = '';
+
+	for (var i = 0, len = album.songs.length; i < len; i++) {
+		albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+	}
+};
+
+window.onload = function() {
+	var allAlbums = [albumPicasso,albumMarconi,albumDivers];
+		count = 0;
+		totalAlbums = allAlbums.length - 1;
+	
+	setCurrentAlbum(allAlbums[count]);
+
+	var albumCover = document.getElementsByClassName('album-cover-art')[0];
+	
+	albumCover.addEventListener('click', function(){
+		if(count > totalAlbums){
+			count = 0;
+		}
+		setCurrentAlbum(allAlbums[count]);
+		count++;
+	});
+};
