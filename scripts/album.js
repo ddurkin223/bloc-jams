348d1cc3a2b7f1c7cb9c8324d2df0441c2b74ff8
Dan Durkin <daniel.h.durkin@gmail.com>
2015-12-06 11:40:32 -0500

    Completed Assignment 33

--- scripts/album.js
+++ scripts/album.js
@@ -175,6 +175,23 @@ var setVolume = function (volume){
 	}
 };
 
+var togglePlayFromPlayerBar = function () {
+	if(!currentSoundFile){
+		return null;
+	}
+	
+	if(currentSoundFile.isPaused()){
+		getSongNumberCell(currentlyPlayingSongNumber).html(pauseButtonTemplate);
+		$(this).html(playerBarPauseButton);
+		currentSoundFile.play();
+	}
+	else{
+		getSongNumberCell(currentlyPlayingSongNumber).html(playButtonTemplate);
+		$(this).html(playerBarPlayButton);
+		currentSoundFile.pause();
+	}
+};
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>'
@@ -188,9 +205,11 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next')
+var $playPauseButton = $('.main-controls .play-pause')
 
 $(window).load(function(){
 	setCurrentAlbum(albumPicasso);
 	$previousButton.click(previousSong);
 	$nextButton.click(nextSong);
+	$playPauseButton.click(togglePlayFromPlayerBar);
 });
